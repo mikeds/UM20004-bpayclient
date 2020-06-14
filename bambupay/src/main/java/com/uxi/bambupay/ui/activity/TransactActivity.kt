2d43976d26a021bfce5d26e5171cb07c8185fad4
Eraño Payawal <hunterxer31@gmail.com>
2020-06-14 19:57:25 +0800

    Added Transact Screen UI

---
+++ bambupay/src/main/java/com/uxi/bambupay/ui/activity/TransactActivity.kt
@@ -0,0 +1,44 @@
+package com.uxi.bambupay.ui.activity
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.core.content.ContextCompat
+import com.uxi.bambupay.R
+import kotlinx.android.synthetic.main.app_toolbar.*
+
+class TransactActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setupToolbar()
+    }
+
+    override fun getLayoutId() = R.layout.activity_transact
+
+    override fun finish() {
+        super.finish()
+        overridePendingTransition(R.anim.from_left_in, R.anim.from_right_out)
+    }
+
+    override fun onBackPressed() {
+        finish()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(toolbar)
+        tv_toolbar_title?.text = getString(R.string.transact)
+//        toolbar.overflowIcon = ContextCompat.getDrawable(this, R.drawable.ic_more)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back)
+    }
+}
--- bambupay/src/main/java/com/uxi/bambupay/ui/home/HomeFragment.kt
+++ bambupay/src/main/java/com/uxi/bambupay/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.uxi.bambupay.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,7 +11,10 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.amulyakhare.textdrawable.TextDrawable
+import com.uxi.bambupay.MainActivity
 import com.uxi.bambupay.R
+import com.uxi.bambupay.ui.activity.TransactActivity
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -30,4 +34,15 @@ class HomeFragment : Fragment() {
         })
         return root
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btn_pay.setOnClickListener {
+            val intent = Intent(activity, TransactActivity::class.java)
+            startActivity(intent)
+            activity?.overridePendingTransition(R.anim.from_right_in, R.anim.from_left_out)
+        }
+    }
+
 }
